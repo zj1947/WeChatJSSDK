4202ac92905c79b088a5bebf86051e14009f2b70
Zengjiang <zj1947@outlook.com>
2015-04-24 11:32:25 +0800

    服务层修改，转发给dao层处理

--- WeChatJSSDK/app/src/main/assets/jssdk.js
+++ WeChatJSSDK/app/src/main/assets/jssdk.js
@@ -1,6 +1,91 @@
 
 
-var wx={
+    function websiteReq(interfaceNm,parameter,a){
+        console.log("JSBridge clientReturn interfaceNm:"+interfaceNm);
+         WxJSBridge.websiteReq(interfaceNm,parameter,function(result){clientReturn(interfaceNm,result,a);})
+
+    }
+    function clientReturn(interfaceNm,result,a){
+       var jo=toJson(result);
+        console.log("JSBridge clientReturn:"+JSON.stringify(jo));
+        console.log("JSBridge clientReturn interfaceNm:"+interfaceNm);
+       var responseStatus=(jo.errMsg).substring(interfaceNm.length+1);
+        switch(responseStatus){
+            case "ok":
+                a.success&&a.success(jo);
+                break;
+            case "cancel":
+                 a.cancel&&a.cancel(jo);
+                break;
+            default:a.fail&&a.fail(jo)
+        };
+    }
+
+    function toJson(obj){
+        if(typeof(obj)=='string'){
+            return JSON.parse( obj );
+        }else{
+            return obj;
+        }
+    }
+    function ParseObject(obj){
+            var strJson=Serialize(obj);
+            console.log("JSBridge ParseObject:"+strJson);
+          return toJson(strJson);
+    }
+    function Serialize(obj) {
+        switch (obj.constructor) {
+        case Object:
+            var str = "{";
+            for (var o in obj) {
+                if('success'==o||'fail'==o||'cancel'==o){
+                            continue;
+                }
+                str += '\"' + o + "\":\"" + Serialize(obj[o]) + "\","
+            }
+            if (str.substr(str.length - 1) == ",") str = str.substr(0, str.length - 1);
+            return str + "}";
+            break;
+        case Array:
+            var str = "[";
+            for (var o in obj) {
+                str += Serialize(obj[o]) + ","
+            }
+            if (str.substr(str.length - 1) == ",") str = str.substr(0, str.length - 1);
+            return str + "]";
+            break;
+        case Boolean:
+            return  obj.toString();
+            break;
+        case Date:
+            return obj.toString();
+            break;
+        case Function:
+            break;
+        case Number:
+            return obj.toString();
+            break;
+        case String:
+            return obj.toString();
+            break
+        }
+    }
+
+    function empty(v){
+    switch (typeof v){
+        case 'undefined' : return true;
+        case 'string' : if(trim(v).length == 0) return true; break;
+        case 'boolean' : if(!v) return true; break;
+        case 'number' : if(0 === v) return true; break;
+        case 'object' :
+        if(null === v) return true;
+        if(undefined !== v.length && v.length==0) return true;
+        for(var k in v){return false;} return true;
+        break;
+    }
+    return false;
+}
+    var wx={
         getLocation:function(a){
             websiteReq('getLocation',
                             ParseObject(a),
@@ -26,89 +111,13 @@ var wx={
                             ParseObject(a),
                              a);
         },
+        getNetworkType:function(a){
+            websiteReq('getNetWorkType',
+                            ParseObject(a),
+                             a);
+        },
         test:function(b){
             websiteReq('test',ParseObject(b),b);
         }
-};
-
-function websiteReq(interfaceNm,parameter,a){
-     WxJSBridge.websiteReq(interfaceNm,parameter,function(result){clientReturn(interfaceNm,result,a);})
-}
-function clientReturn(interfaceNm,result,a){
-   var jo=toJson(result);
-   var responseStatus=(jo.errMsg).substring(interfaceNm.length+1);
-    switch(responseStatus){
-        case "ok":
-            a.success&&a.success(jo);
-            break;
-        case "cancel":
-             a.cancel&&a.cancel(jo);
-            break;
-        default:a.fail&&a.fail(jo)
-    };
 }
 
-function toJson(obj){
-    if(typeof(obj)=='string'){
-        return JSON.parse( obj );
-    }else{
-        return obj;
-    }
-};
-function ParseObject(obj){
-        var strJson=Serialize(obj);
-        console.log("BabJSBridge"+strJson);
-      return toJson(strJson);
-};
-function Serialize(obj) {
-	switch (obj.constructor) {
-	case Object:
-		var str = "{";
-		for (var o in obj) {
-		    if('success'==o||'fail'==o||'cancel'==o){
-        		        continue;
-            }
-			str += '\"' + o + "\":\"" + Serialize(obj[o]) + "\","
-		}
-		if (str.substr(str.length - 1) == ",") str = str.substr(0, str.length - 1);
-		return str + "}";
-		break;
-	case Array:
-		var str = "[";
-		for (var o in obj) {
-			str += Serialize(obj[o]) + ","
-		}
-		if (str.substr(str.length - 1) == ",") str = str.substr(0, str.length - 1);
-		return str + "]";
-		break;
-	case Boolean:
-		return  obj.toString();
-		break;
-	case Date:
-		return obj.toString();
-		break;
-	case Function:
-		break;
-	case Number:
-		return obj.toString();
-		break;
-	case String:
-		return obj.toString();
-		break
-	}
-};
-
-function empty(v){
-    switch (typeof v){
-        case 'undefined' : return true;
-        case 'string' : if(trim(v).length == 0) return true; break;
-        case 'boolean' : if(!v) return true; break;
-        case 'number' : if(0 === v) return true; break;
-        case 'object' :
-        if(null === v) return true;
-        if(undefined !== v.length && v.length==0) return true;
-        for(var k in v){return false;} return true;
-        break;
-    }
-    return false;
-}
--- WeChatJSSDK/app/src/main/java/com/z/wechatjssdk/webview/service/impl/BaseServiceImpl.java
+++ WeChatJSSDK/app/src/main/java/com/z/wechatjssdk/webview/service/impl/BaseServiceImpl.java
@@ -2,18 +2,25 @@ package com.z.wechatjssdk.webview.service.impl;
 
 import com.z.wechatjssdk.webview.bean.Request;
 import com.z.wechatjssdk.webview.bean.Response;
+import com.z.wechatjssdk.webview.dao.DaoFactory;
+import com.z.wechatjssdk.webview.dao.IDao;
 import com.z.wechatjssdk.webview.service.IEventService;
 import com.z.wechatjssdk.webview.service.IOnServiceFinish;
 
 import org.json.JSONObject;
 
 /**
- * Created by Administrator on 15-4-22.
+ * Created by Administrator on 15-4-23.
  */
-public class ToastServiceImpl implements IEventService {
+public class BaseServiceImpl implements IEventService {
     @Override
     public void getResponse(Request request, IOnServiceFinish listener) {
-        Response response=new Response(request.getInterfaceNm(),request.getRequestJSON());
+
+        String interfaceNm=request.getInterfaceNm();
+
+        IDao dao= DaoFactory.produceDao(interfaceNm);
+        Response response=dao.getResponseJSON(request);
+
         listener.onServiceFinish(response);
     }
 }
