9cadf85ce613ac9107c071e42aeddad2baa89557
Zengjiang <zj1947@outlook.com>
2015-04-27 11:07:08 +0800

    更改为线程处理web端请求

--- WeChatJSSDK/app/src/main/java/com/z/wechatjssdk/webview/service/manager/impl/ServiceManagerImpl.java
+++ WeChatJSSDK/app/src/main/java/com/z/wechatjssdk/webview/service/manager/impl/ServiceManagerImpl.java
@@ -1,5 +1,8 @@
 package com.z.wechatjssdk.webview.service.manager.impl;
 
+import android.os.Handler;
+import android.os.Looper;
+
 import com.z.wechatjssdk.webview.bean.Request;
 import com.z.wechatjssdk.webview.bean.Response;
 import com.z.wechatjssdk.webview.service.ServiceFactory;
@@ -12,13 +15,31 @@ import com.z.wechatjssdk.webview.service.manager.IOnServiceFinish;
  */
 public class ServiceManagerImpl implements IServiceManager {
     @Override
-    public void getResponse(Request request, IOnServiceFinish listener) {
+    public void getResponse(final Request request, final IOnServiceFinish listener) {
 
-        String interfaceNm=request.getInterfaceNm();
 
-        IService service= ServiceFactory.produceService(interfaceNm);
-        Response response=service.getResponseJSON(request);
 
-        listener.onServiceFinish(response);
+        new Thread(){
+            @Override
+            public void run() {
+                super.run();
+
+                String interfaceNm=request.getInterfaceNm();
+
+                IService service= ServiceFactory.produceService(interfaceNm);
+                final Response response=service.getResponseJSON(request);
+
+
+                //获取主线程的handler
+                Handler handler=new Handler(Looper.getMainLooper());
+                //在主线程中处理结果
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        listener.onServiceFinish(response);
+                    }
+                });
+            }
+        }.start();
     }
 }
