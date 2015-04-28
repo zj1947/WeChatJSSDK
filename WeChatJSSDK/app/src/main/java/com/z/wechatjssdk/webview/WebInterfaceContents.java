d9aa4b02ed1ffa17389d2c38b133d64b75630976
Zengjiang <zj1947@outlook.com>
2015-04-28 16:33:55 +0800

    删除一些接口

--- WeChatJSSDK/app/src/main/java/com/z/wechatjssdk/webview/WebInterfaceContents.java
+++ WeChatJSSDK/app/src/main/java/com/z/wechatjssdk/webview/WebInterfaceContents.java
@@ -8,12 +8,8 @@ public class WebInterfaceContents {
     public static final String ERR_MSG="errMsg";
     public static final String INJECTED_NAME = "WxJSBridge";
     public static final String INTERFACE_NM_GET_LOCATION="getLocation";
-    public static final String INTERFACE_NM_PRE_IMG="previewImage";
-    public static final String INTERFACE_NM_UPLOAD_IMG="uploadImage";
     public static final String INTERFACE_NM_CHOOSE_IMG="chooseImage";
-    public static final String INTERFACE_NM_GO_BACK="goBack";
-    public static final String INTERFACE_NM_ALERT="alert";
-    public static final String INTERFACE_NM_TOAST="toast";
     public static final String INTERFACE_NM_GET_NETWORK_TYPE="getNetWorkType";
+    public static final String INTERFACE_NM_TOAST="toast";
 
 }
