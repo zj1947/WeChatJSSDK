e81d9ef76e852be1c40151ceb86a49c54779308c
Zengjiang <zj1947@outlook.com>
2015-04-28 16:22:01 +0800

    添加web接口不存在的处理

---
+++ WeChatJSSDK/app/src/main/java/com/z/wechatjssdk/webview/service/impl/InterfaceNotFoundServiceImpl.java
@@ -0,0 +1,21 @@
+package com.z.wechatjssdk.webview.service.impl;
+
+import com.z.wechatjssdk.webview.WebInterfaceContents;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by Administrator on 15-4-28.
+ */
+public class InterfaceNotFoundServiceImpl extends BaseServiceImpl {
+    @Override
+    public void parserReqJSON(JSONObject jsonObject) throws JSONException {
+
+    }
+
+    @Override
+    public void setResultJSON() throws JSONException {
+        mJoResult.put(WebInterfaceContents.ERR_MSG,"接口"+mInterfaceNm+"不存在");
+    }
+}
