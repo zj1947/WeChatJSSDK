005acf94839e2f64edf3855feeaea40df75fad8e
Zengjiang <zj1947@outlook.com>
2015-04-30 11:32:23 +0800

    增加注释

--- WeChatJSSDK/app/src/main/java/com/z/wechatjssdk/webview/js/HostJsScope.java
+++ WeChatJSSDK/app/src/main/java/com/z/wechatjssdk/webview/js/HostJsScope.java
@@ -16,6 +16,12 @@ import org.json.JSONObject;
 //HostJsScope中需要被JS调用的函数，必须定义成public static，且必须包含WebView这个参数
 public class HostJsScope {
 
+    /**
+     * @param webView 装载html的webview，必须包含WebView这个参数
+     * @param interfaceNm 接口名 对应JS的interfaceNm
+     * @param jo 请求参数 对应JS中的parameter
+     * @param jsCallback  对应JS中的function(result)，执行结束会回调这个函数
+     */
     public static void websiteReq(WebView webView,String interfaceNm,JSONObject jo,final JsCallback jsCallback){
     }
 
